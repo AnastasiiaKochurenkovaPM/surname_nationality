4575c91ffb535407f685175306a618ee9afa6a1a
AnastasiiaKochurenkovaPM <anastasiia.kochurenkova.pm.2020@lpnu.ua>
2023-12-12 17:22:24 +0200

    last version

--- Kursova_2/server/predict.py
+++ Kursova_2/server/predict.py
@@ -13,23 +13,27 @@ def evaluate(line_tensor):
     
     return output
 
-def predict(line, n_predictions=1):
+def predict(line, n_predictions=10):
     output = evaluate(Variable(lineToTensor(line)))
 
-    # Get top N categories
     topv, topi = output.data.topk(n_predictions, 1, True)
+    threshold = 0.5
     predictions = []
 
     for i in range(n_predictions):
-        value = topv[0][i]
+        current_value = topv[0][i]
+        previous_value = topv[0][i - 1]
         category_index = topi[0][i]
+        
+        if previous_value - current_value > threshold:
+            break  
+        
         print((all_categories[category_index]))
-        predictions.append([value, all_categories[category_index]])
+        predictions.append([current_value, all_categories[category_index]])
 
     return predictions
 
 if __name__ == '__main__':
-    # Check if there is at least one command-line argument
     if len(sys.argv) > 1:
         predict(sys.argv[1])
     else:
